9e4d4743bcde03abab8194de3d97056fbf942b0f
folly <folly91@163.com>
2020-07-23 14:46:08 +0800

    添加vdom

--- YYReact.js
+++ YYReact.js
@@ -1,58 +1,91 @@
+let childrenSymbol = Symbol("children");
 class ElementWarpper {
   constructor(type) {
-    this.root = document.createElement(type);
+    this.type = type;
+    this.props = Object.create(null);
+    this[childrenSymbol] = [];
+    this.children = [];
   }
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
-      this.root.addEventListener(eventName, value);
-    }
-    if (name === 'className') {
-      name = 'class';
-    }
-    this.root.setAttribute(name, value);
+    this.props[name] = value;
   }
   appendChild(vchild) {
-    let range = document.createRange();
-    if(this.root.children.length) {
-      range.setStartAfter(this.root.lastChild)
-      range.setEndAfter(this.root.lastChild)
-    } else {
-      range.setStart(this.root, 0)
-      range.setEnd(this.root, 0)
-    }
-    vchild.mountTo(range)
+    this[childrenSymbol].push(vchild);
+    this.children.push(vchild.vdom);
+  }
+  get vdom() {
+    return this;
   }
   mountTo(range) {
-    range.deleteContents()
-    range.insertNode(this.root)
+    this.range = range;
+    let placeholder = document.createComment("placeholder");
+    let endRange = document.createRange();
+    endRange.setStart(range.endContainer, range.endOffset);
+    endRange.setEnd(range.endContainer, range.endOffset);
+    endRange.insertNode(placeholder);
+    range.deleteContents();
+    let element = document.createElement(this.type);
+    for (let name in this.props) {
+      let value = this.props[name];
+      if (name.match(/^on([\s\S]+)$/)) {
+        let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+        element.addEventListener(eventName, value);
+      }
+      if (name === "className") {
+        name = "class";
+      }
+      element.setAttribute(name, value);
+    }
+    for (let child of this.children) {
+      let range = document.createRange();
+      if (element.children.length) {
+        range.setStartAfter(element.lastChild);
+        range.setEndAfter(element.lastChild);
+      } else {
+        range.setStart(element, 0);
+        range.setEnd(element, 0);
+      }
+      child.mountTo(range);
+    }
+    range.insertNode(element);
   }
 }
 
 class TextWarpper {
-  constructor(type) {
-    this.root = document.createTextNode(type);
+  constructor(content) {
+    this.root = document.createTextNode(content);
+    this.type = "#text";
+    this.children = [];
+    this.props = Object.create(null);
   }
   mountTo(range) {
-    range.deleteContents()
-    range.insertNode(this.root)
+    this.range = range;
+    range.deleteContents();
+    range.insertNode(this.root);
+  }
+  get vdom() {
+    console.log(this);
+    return this;
   }
 }
 
 export class Component {
   constructor() {
     this.children = [];
-    this.props = Object.create(null)
+    this.props = Object.create(null);
     this.eventObj = {
-      componentWillUpdate:null,
-      componentDidUpdate:this.componentDidUpdate
-    }
+      componentWillUpdate: null,
+      componentDidUpdate: this.componentDidUpdate,
+    };
     if (this.componentWillUpdate) {
       this.eventObj.componentWillUpdate = this.componentWillUpdate;
-    } 
-    if (this.componentDidUpdate) {
-      this.eventObj.componentDidUpdate = this.componentDidUpdate
     }
+    if (this.componentDidUpdate) {
+      this.eventObj.componentDidUpdate = this.componentDidUpdate;
+    }
+  }
+  get type() {
+    return this.constructor.name;
   }
   setAttribute(name, value) {
     this.props[name] = value;
@@ -61,20 +94,71 @@ export class Component {
   mountTo(range) {
     this.componentWillMount && this.componentWillMount();
     this.range = range;
-    this.update(); 
-    this.componentDidMount && this.componentDidMount();   
+    this.update();
+    this.componentDidMount && this.componentDidMount();
   }
   update() {
-    this.eventObj.componentWillUpdate&&this.eventObj.componentWillUpdate()
-    let range = document.createRange();
-    let placeholder = document.createComment('placeholder')
-    range.setStart(this.range.endContainer,this.range.endOffset)
-    range.setEnd(this.range.endContainer,this.range.endOffset)
-    range.insertNode(placeholder)
-    this.range.deleteContents()
-    let vdom = this.render();
-    vdom.mountTo(this.range);
-    this.eventObj.componentDidUpdate&&this.eventObj.componentDidUpdate()
+    let vdom = this.vdom;
+    if (this.oldVdom) {
+      let isSameNode = (node1, node2) => {
+        if (node1.type !== node2.type) {
+          return false;
+        }
+        for (let name in node1.props) {
+          if (
+            typeof node1.props[name] === "object" &&
+            typeof node2.props[name] === "object" &&
+            JSON.stringify(
+              node1.props[name] === JSON.stringify(node2.props[name])
+            )
+          ) {
+            continue;
+          }
+          if (node1.props[name] !== node2.props[name]) {
+            return false;
+          }
+        }
+        if (
+          Object.keys(node1.props).length !== Object.keys(node2.props).length
+        ) {
+          return false;
+        }
+        return true;
+      };
+      let isSameTree = (node1, node2) => {
+        if (!isSameNode(node1, node2)) {
+          return false;
+        }
+        if (node1.children.length !== node2.children.length) {
+          return false;
+        }
+        for (let i = 0; i < node1.children.length; i++) {
+          if (!isSameTree(node1.children[i], node2.children[i])) {
+            return false;
+          }
+        }
+        return true;
+      };
+      let replace = (newTree, oldTree) => {
+        if (isSameTree(newTree, oldTree)) {
+          return;
+        }
+        if (!isSameNode(newTree, oldTree)) {
+          newTree.mountTo(oldTree.range);
+        } else {
+          for (let i = 0; i < newTree.children.length; i++) {
+            replace(newTree.children[i], oldTree.children[i]);
+          }
+        }
+      };
+      replace(vdom, this.oldVdom);
+    } else {
+      vdom.mountTo(this.range);
+    }
+    this.oldVdom = vdom;
+  }
+  get vdom() {
+    return this.render().vdom;
   }
   appendChild(vchild) {
     this.children.push(vchild);
@@ -82,21 +166,24 @@ export class Component {
   setState(state) {
     let merge = (oldState, newState) => {
       for (let p in newState) {
-        if (typeof newState[p] ==='object') {
-          if (typeof oldState[p] !== 'object') {
-            oldState[p] = {}
+        if (typeof newState[p] === "object" && newState[p] !== null) {
+          if (newState[p] instanceof Array) {
+            oldState[p] = [];
           }
-          merge(oldState[p], newState[p])
+          if (typeof oldState[p] !== "object") {
+            oldState[p] = {};
+          }
+          merge(oldState[p], newState[p]);
         } else {
-          oldState[p]= newState[p]
+          oldState[p] = newState[p];
         }
       }
-    }
+    };
     if (!this.state && state) {
-      this.state = {}
+      this.state = {};
     }
-    merge(this.state, state)
-    this.update()
+    merge(this.state, state);
+    this.update();
   }
 }
 
@@ -116,7 +203,14 @@ export let YYReact = {
         if (typeof child === "object" && child instanceof Array) {
           insertChildren(child);
         } else {
-          if (!(child instanceof Component) && !(child instanceof ElementWarpper) && !(child instanceof TextWarpper)) {
+          if (child === null || child === void 0) {
+            child = "";
+          }
+          if (
+            !(child instanceof Component) &&
+            !(child instanceof ElementWarpper) &&
+            !(child instanceof TextWarpper)
+          ) {
             child = String(child);
           }
           if (typeof child === "string") {
@@ -131,12 +225,12 @@ export let YYReact = {
   },
   render(vdom, element) {
     let range = document.createRange();
-    if(element.children.length) {
-      range.setStartAfter(element.lastChild)
-      range.setEndAfter(element.lastChild)
+    if (element.children.length) {
+      range.setStartAfter(element.lastChild);
+      range.setEndAfter(element.lastChild);
     } else {
-      range.setStart(element,0)
-      range.setEnd(element, 0)
+      range.setStart(element, 0);
+      range.setEnd(element, 0);
     }
     vdom.mountTo(range);
   },
